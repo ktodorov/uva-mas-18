d795ddee993037e1f29b2c710355bbd26666c507
David Speck <david@saduras.de>
2018-11-03 14:25:02 +0100

    Plot policy

--- 6-8/main.py
+++ 6-8/main.py
@@ -2,8 +2,11 @@ from gridworld import GridWorld
 import random
 from collections import defaultdict
 import numpy as np
+import matplotlib
+matplotlib.use('TkAgg')
 import matplotlib.pyplot as plt
 from plot_utils import heatmap, annotate_heatmap
+import math
 
 def get_random_start_pos(gridworld):
     width, height = len(gridworld.grid), len(gridworld.grid[0])
@@ -42,12 +45,98 @@ def qDictToMatrix(Q, gridworld):
 
     return m
 
+def qDictToPolicy(Q, gridworld):
+    width, height = len(gridworld.grid), len(gridworld.grid[0])
+    m = np.empty((width, height))
+
+    for x in range(width):
+        for y in range(height):
+            state = (x,y)
+            qs = [Q[((state), 0)], Q[(state, 1)], Q[(state, 2)], Q[(state, 3)]]
+            m[x][y] = np.argmax(qs)
+
+            r = gridworld.get_state_reward(state)
+            if(r != gridworld.move_value):
+                m[x][y] = None
+
+            if(gridworld.grid[x][y] == '#'):
+                m[x][y] = None
+
+    return m
+
 def plotQ(Q, gridworld, title=''):
     m = qDictToMatrix(Q, gridworld)
 
     plt.title(title)
     im, _ = heatmap(m.transpose())
     annotate_heatmap(im)
+    plt.show()
+
+def plotArrow(x, y, action):
+    color = 'black'
+
+    dx = 0
+    dy = 0
+    arrowWidth = 0.15
+    arrowLength = 0.3
+    arrowHeadWidth = 0.35
+    arrowHeadLength = 0.25
+
+    x = x 
+    y = y + 1
+
+    if action == 0:
+        dx = 0
+        dy = arrowLength
+        x = x - (arrowLength + arrowHeadLength) / 2
+    elif action == 1:
+        dx = arrowLength
+        dy = 0
+        arrowWidth = arrowWidth
+        arrowHeadWidth = arrowHeadWidth / 1.2
+        y = y - (arrowWidth + arrowHeadWidth) / 2
+    elif action == 2:
+        dx = 0
+        dy = -arrowLength
+        x = x + (arrowLength + arrowHeadLength) / 2
+    elif action == 3:
+        dx = -arrowLength
+        dy = 0
+        arrowWidth = arrowWidth
+        arrowHeadWidth = arrowHeadWidth / 1.2
+        y = y + (arrowWidth + arrowHeadWidth) / 2
+    
+    plt.arrow(y, x, dx, dy, head_width=arrowHeadWidth, head_length=arrowHeadLength, width=arrowWidth, color=color)
+
+def plotPolicy(Q, gridworld, title = ""):
+    m = qDictToPolicy(Q, gridworld)
+    height, width = len(m), len(m[0])
+
+    for x in range(width):
+        for y in range(height):
+            if(not math.isnan(m[x][y])):
+                plotArrow(height - y, x, m[x][y])
+            if(gridworld.grid[x][y] == 'G' or gridworld.grid[x][y] == 'P'):
+                plt.text(x + 0.75, height - y - 0.15, gridworld.grid[x][y], fontsize = 25)
+
+    xTicks = np.arange(0, width + 2, 1)
+    subXTicks = np.arange(0.5, width + 1.5, 1)
+    yTicks = np.arange(0, height + 2, 1)
+    subYTicks = np.arange(0.5, height + 1.5, 1)
+
+    axes = plt.gca()
+    axes.set_xticks(xTicks)
+    axes.set_xticks(subXTicks, minor=True)
+    axes.set_yticks(yTicks)
+    axes.set_yticks(subYTicks, minor=True)
+    
+    axes.set_xlim([0.5, width + 0.5])
+    axes.set_ylim([0.5, height + 0.5])
+    
+
+    axes.grid(which='minor', alpha=0.5)
+    plt.title(title)
+    
     plt.show()
 
 def SARSA(gridworld, episodes = 100, eps = 0.1, lr = 0.1, g = 1):
@@ -134,16 +223,18 @@ def main():
         grid = file.read()
 
     eps = 0.2
-    episodes = 100
+    episodes = 1000
 
     random.seed(1)
     gw = GridWorld(grid)
     Q = SARSA(gw, episodes=episodes, eps=eps)
-    plotQ(Q, gw, f'SARSA after {episodes} episodes')
+    # plotQ(Q, gw, f'SARSA after {episodes} episodes')
+    plotPolicy(Q, gw, f'SARSA: greedy-policy after {episodes} episodes')
 
     random.seed(1)
     Q = QLearning(gw, episodes=episodes, eps=eps)
-    plotQ(Q, gw, f'Q-Learning after {episodes} episodes')
+    # plotQ(Q, gw, f'Q-Learning after {episodes} episodes')
+    plotPolicy(Q, gw, f'Q-Learning: greedy-policy after {episodes} episodes')
 
 if __name__ == '__main__':
     main()
